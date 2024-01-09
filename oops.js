21d4b19f0985d16966ed32eea726b2155c98d3ba
Shanmugamsham <abdeshanmugam@gmail.com>
2024-01-09 18:04:06 +0700

    new

---
+++ oops.js
@@ -0,0 +1,130 @@
+// 1// Class - Movie
+
+
+// class Movie{
+//     constructor(title,studio,rating){
+//         this.title=title
+//         this.studio=studio
+//         this.rating=rating
+//         }
+        
+// }
+
+// var new_movie_1= new Movie("Casino Royale","Eon Productions","PG­13")
+// var avenger=new Movie("Endgame","marvel","PG11")
+// console.log(new_movie_1,avenger)
+
+
+//2// circle
+
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius
+//         this.color=color
+        
+//         }
+//         getdetail(){
+//             return `this is radiou of circle value ${this.radius}`
+//         }
+//         set chagecolor(modification){
+//             this.color=modification
+//         }
+
+
+// }
+
+// //circle()
+// var cicle_1=new circle(1.0,"red")
+// console.log(cicle_1);
+
+// //circle_radiouss
+// console.log(cicle_1.radius)
+
+// //circle_radious and color
+// console.log(cicle_1.radius,cicle_1.color);
+
+// //get method
+// console.log(cicle_1.getdetail())
+
+// //set method
+
+// cicle_1.chagecolor="green"
+// console.log(cicle_1.color);
+
+
+
+//3 //
+
+
+// class person{
+//     constructor(name,age,gender,city,education,mai_id,number){
+//         this.name=name
+//         this.age=age
+//         this.gender=gender
+//         this.city=city
+//         this.education=education
+//         this.mai_id=mai_id
+//         this.number=number
+        
+//         }
+//         getname(){
+//             return `my name is ${this.name}`
+            
+//         }
+
+//         getmail_id(){
+//             return `my mai_id is ${this.mai_id}`
+            
+//         }
+
+
+//         getcity(){
+//             return `i am from ${this.city}`
+            
+//         }
+        
+//         set chageage(modification){
+//             this.age=modification
+//         }
+
+
+// }
+
+// var person_1=new person("shanmugasundaram",25,"male","udumalai","B.com","abdeshanmugam",995252)
+// var person_2=new person("kannan",20,"male","tirupur","b com","kanna@sss",25468576)
+
+// console.log(person_1)
+// console.log(person_1.name)
+// console.log(person_1.getcity())
+
+// person_1.chageage=28
+// console.log(person_1.age)
+
+
+
+
+
+// class uber{
+
+//    constructor(customer_name,customer_location,customer_drobing_location,distance){
+//        this.customer_name
+//        this.customer_location
+//        this.customer_drobing_location
+//        this.distance
+       
+        
+//         }
+//         getprice(){
+//             return `total amount ${this.distance} RS`
+//         }
+
+//         set price(modification){
+//             this.price=modification
+
+//         }
+//     }
+
+
+//     var ride_1=new uber("shanmugam","udumalai","coimbotor",5)
+
+//     console.log(ride_1.getprice())
